1ddfe89b350179eb52ba414531dde158052c793c
Erik Maquera <59391741+Erik-Beckhs@users.noreply.github.com>
2021-11-23 23:07:26 -0400

    send message to server

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
+# Socket Server
+
 Reconstruir modulos de node
+```
 npm install
+```
 
 Generar el DIST
+```
 tsc -w
+```
 
 INICIAR EL SERVIDOR (cualquiera de los dos)
+```
 node dist/
 nodemon dist/
+```
--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,54 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import * as socket from '../sockets/socket';
+import socketIO from 'socket.io';
+//socketIO = require('socket.io')
+
+import http from 'http';
 
 export default class Server {
+    private static _instance:Server;
+
     public app:express.Application;
     public port:number;
 
-    constructor(){
-        this.app = express()
-        this.port = SERVER_PORT
+    public io : socketIO.Server;
+
+
+    private httpServer : http.Server;
+
+    private constructor(){
+        this.app = express();
+        this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = require('socket.io')(this.httpServer, {
+            cors: {
+                origin: "http://localhost:4200"
+        }}); //reemplaza a lo siguiente
+        //this.io = socketIO(this.httpServer);
+        this.escucharSockets()
+    }
+
+    public static get instance(){
+        return this._instance || (this._instance = new this())
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            socket.mensaje(cliente);
+            //desconectar
+            //const desconectar:(cliente:socketIO.socket) ==> void
+            socket.desconectar(cliente)
+        });
     }
 
     start (callback:any){
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 
 }
